1f603779c6e46e716498f7b1df4198b6873141e1
Valeria Del Río <vdelrio@grupoassa>
2018-06-12 15:17:27 -0300

    Usuario y token listo}

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion  a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -13,16 +24,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 // Rutas
-
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    ok: true,
-    message: 'Petición realizada correctamente'
-  })
-  console.log(res)
-})
-
-// Escuchar peticiones
+app.use('/usuario', usuarioRoutes)
+app.use('/login', loginRoutes)
+app.use('/', appRoutes)
+  // Escuchar peticiones
 
 app.listen(3000, () => {
   console.log('express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
---
+++ routes/usuario.js
@@ -0,0 +1,143 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var mdAutenticacion = require('../middlewares/autenticacion');
+var app = express();
+
+var Usuario = require('../models/usuario');
+
+// ===================================
+// Obtener todos los usuarios
+// ===================================
+app.get('/', (req, res, next) => {
+  Usuario.find({}, 'nombre email img rol')
+    .exec(
+      (err, usuarios) => {
+        if (err) {
+          res.status(500).json({
+            ok: false,
+            message: 'Error cargando usuarios',
+            errors: err
+          })
+        }
+
+        res.status(200).json({
+          ok: true,
+          usuarios: usuarios
+        });
+      })
+
+});
+
+
+// ===================================
+// Actualizar usuario
+// ===================================
+
+app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
+  var id = req.params.id;
+  var body = req.body;
+  Usuario.findById(id, (err, usuario) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: 'Error al buscar usuario',
+        errors: err
+      })
+    }
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        message: 'El usuario con el id ' + id + ' no existe',
+        errors: { message: 'No existe un usuario con ese ID' }
+      })
+    }
+
+    usuario.nombre = body.nombre;
+    usuario.email = body.email;
+    usuario.rol = body.rol;
+
+    usuario.save((err, usuarioGuardado) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          message: 'Error al actualizar usuario',
+          errors: err
+        })
+      }
+      res.status(200).json({
+        ok: true,
+        usuario: usuarioGuardado
+      });
+    });
+  })
+
+})
+
+// ===================================
+// Crear un nuevo usuario
+// ===================================
+
+app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+  var body = req.body;
+
+  var usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    img: body.img,
+    rol: body.rol
+  });
+
+  usuario.save((err, usuarioGuardado) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        message: 'Error al crear usuario',
+        errors: err
+      })
+    }
+    res.status(201).json({
+      ok: true,
+      usuario: usuarioGuardado,
+      usuarioToken: req.usuario
+    });
+  });
+
+
+})
+
+// ===================================
+// Eliminar un usuario por id
+// ===================================
+
+app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+  var id = req.params.id;
+  Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: 'Error al eliminar usuario',
+        errors: err
+      })
+    }
+
+    if (!usuarioBorrado) {
+      return res.status(400).json({
+        ok: false,
+        message: 'No existe un usuario con ese id',
+        errors: { message: 'No existe un usuario con ese id' }
+      })
+    }
+
+    res.status(200).json({
+      ok: true,
+      usuario: usuarioBorrado
+    });
+
+  })
+
+})
+module.exports = app;
