a765056d015d456f5d506f2b05904afb609a53e4
Valeria Del Río <vdelrio@grupoassa>
2018-07-24 13:40:33 -0300

    Sección 11 terminada - CRUD -Imagenes

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
+
+
 
 
 // Conexion  a la base de datos
@@ -31,8 +36,13 @@ app.use('/usuario', usuarioRoutes)
 app.use('/login', loginRoutes)
 app.use('/hospital', hospitalRoutes)
 app.use('/medico', medicoRoutes)
+app.use('/busqueda', busquedaRoutes)
+app.use('/upload', uploadRoutes)
+app.use('/img', imagenesRoutes)
+
 app.use('/', appRoutes)
-  // Escuchar peticiones
+
+// Escuchar peticiones
 
 app.listen(3000, () => {
   console.log('express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
